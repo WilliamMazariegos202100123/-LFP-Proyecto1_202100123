7defbe1adf38380573ead81e0c0762e0de9de324
WilliamMazariegos202100123 <williammazariegos6@gmail.com>
2022-09-25 19:12:55 -0600

    version 6

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from tkinter import messagebox
 from tkinter.ttk import Treeview
 from tkPDFViewer import tkPDFViewer as pdf
 from tokens import Token, Error
-import time
 import re
 import webbrowser
+import numpy
+import math
 row = 0
 col = 0
 a, b = 'áéíóúÁÉÍÓÚ', 'aeiouAEIOU'
@@ -82,6 +83,8 @@ def manualtecnico():
 def abrirarchivo():
     window_main.withdraw()
     global abrir_win, tokens_lista, errores
+    tokens_lista.clear()
+    errores.clear()
     abrir_win = Toplevel(window_main)
     abrir_win.iconbitmap("icono.ico")
     abrir_win.resizable(width=False, height=False)
@@ -455,10 +458,346 @@ def analizar():
 
 
 def operaciones():
+    print("lista errores: ",len(errores))
     if len(errores)>0:
         messagebox.showerror("Html Failed","No se puede generar un html porque el archivo presenta errores, verifique su archivo de entrada.")
     else:
-        messagebox.showinfo("","tas bien")
+        htmloperacion=""
+        cont=0
+        titulo=""
+        sizetitulo=""
+        colortitulo=""
+        descripcion=""
+        sizedescripcion=""
+        colordescripcion=""
+        sizecontenido=""
+        colorcontenido=""
+        for i in tokens_lista:
+            a=str(i.lexema).lower()
+            if a=='titulo' and len(titulo)==0:
+                titulo+=tokens_lista[cont+2].lexema
+            elif a=='titulocolor' and len(colortitulo)==0:
+                colortitulo+=str(tokens_lista[cont+2].lexema).lower()
+                colortitulo=colortitulo.replace('tamanio','')
+                sizetitulo+=str(tokens_lista[cont+4].lexema)
+            elif a=='texto' and len(descripcion)==0:
+                descripcion+=tokens_lista[cont+2].lexema
+            elif a=='descripcioncolor' and len(colordescripcion)==0:
+                colordescripcion+=str(tokens_lista[cont+2].lexema).lower()
+                colordescripcion=colordescripcion.replace('tamanio','')
+                sizedescripcion+=str(tokens_lista[cont+4].lexema)
+            elif a=='contenidocolor' and len(colorcontenido)==0:
+                colorcontenido+=str(tokens_lista[cont+2].lexema).lower()
+                colorcontenido=colorcontenido.replace('tamanio','')
+                sizecontenido+=str(tokens_lista[cont+4].lexema)
+            cont+=1
+
+        if colorcontenido=='azul':
+            colorcontenido='blue'
+        elif colorcontenido=='rojo':
+            colorcontenido='red'
+        elif colorcontenido=='verde':
+            colorcontenido='green'
+        elif colorcontenido=='amarillo':
+            colorcontenido='red'
+        elif colorcontenido=='gris':
+            colorcontenido='gray'
+        else: 
+            colorcontenido='black'
+
+        if colortitulo=='azul':
+            colortitulo='blue'
+        elif colortitulo=='rojo':
+            colortitulo='red'
+        elif colortitulo=='verde':
+            colortitulo='green'
+        elif colortitulo=='amarillo':
+            colortitulo='red'
+        elif colortitulo=='gris':
+            colortitulo='gray'
+        else: 
+            colortitulo='black'
+
+        if colordescripcion=='azul':
+            colordescripcion='blue'
+        elif colordescripcion=='rojo':
+            colordescripcion='red'
+        elif colordescripcion=='verde':
+            colordescripcion='green'
+        elif colordescripcion=='amarillo':
+            colordescripcion='red'
+        elif colordescripcion=='gris':
+            colordescripcion='gray'
+        else: 
+            colordescripcion='black'
+        
+        htmloperacion+="""
+        <html>
+            <head>
+                <title>Operaciones_202100123</title>
+            </head>
+            <style>
+                .titulo{
+                    font-size: """+str(sizetitulo)+";"+"""
+                    color: """+str(colortitulo)+"""
+                }
+                .descripcion{
+                    font-size:"""+str(sizedescripcion)+";"+"""
+                    color: """+str(colordescripcion)+"""
+                }
+                .contenido{
+                    font-size:"""+str(sizecontenido)+";"+"""
+                    color: """+str(colorcontenido)+"""
+                }
+            </style>
+            <body>
+                <div class="titulo" >"""
+        htmloperacion+="\n\t\t\t\t"+str(titulo)+"""
+                </div>
+                <div class="descripcion">"""
+        htmloperacion+="\n\t\t\t\t"+str(descripcion)+"""
+                </div>
+                <div class="contenido">
+        """
+        print("\n\n")
+        potencia=[]
+        suma=[]
+        resta=[]
+        multi=[]
+        ftrigo=[]
+        div=[]
+        modulos=[]
+        inversos=[]
+        pos=[]
+        raiz=[]
+        cadope=""
+        cont=0
+        for i in tokens_lista:
+            aux=str(i.lexema)
+            c = aux.translate(trans)
+            if c.lower()=='suma': #1
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            suma.append(a)
+                        except:
+                            continue
+            elif c.lower()=='resta': #2
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            resta.append(a)
+                        except:
+                            continue
+            elif c.lower()=='division': #3
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            div.append(a)
+                        except:
+                            continue
+            elif c.lower()=='potencia': #4
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            potencia.append(a)
+                        except:
+                            continue
+
+            elif c.lower()=='multiplicacion': #5
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            multi.append(a)
+                        except:
+                            continue
+            elif c.lower()=='raiz': #6
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=int(tokens_lista[j].lexema)
+                            raiz.append(a)
+                        except:
+                            continue
+
+            elif c.lower()=='mod': #7
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=int(tokens_lista[j].lexema)
+                            modulos.append(a)
+                        except:
+                            continue
+            elif c.lower()=='inverso': #9
+                pos.append(cont)
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=int(tokens_lista[j].lexema)
+                            b=1/a
+                            d=str(a)+"^-1 ="+str(b)
+                            inversos.append(d)
+                        except:
+                            continue
+
+            elif c.lower()=='coseno' or 'seno' or 'tangente': #10
+                pos.append(cont)
+                
+                for j in range(cont,len(tokens_lista)):
+                    if str(tokens_lista[j].lexema).lower()=='operacion':
+                        break
+                    else:
+                        try:
+                            a=float(tokens_lista[j].lexema)
+                            aux=""
+                            if c.lower()=='coseno':
+                                b=math.cos(a)
+                                aux+="Cos("+str(a)+")="+str(b)
+                                ftrigo.append(aux)
+                            elif c.lower()=='seno':
+                                b=math.sin(a)
+                                aux+="Seno("+str(a)+")="+str(b)
+                                ftrigo.append(aux)
+                            elif c.lower()=='tangente':
+                                b=math.tan(a)
+                                aux+="Tan("+str(a)+")="+str(b)
+                                ftrigo.append(aux)
+                        except:
+                            continue
+            cont+=1
+        if len(inversos)>0:
+            htmloperacion+="\t\t\tINVERSOS: <br>\n"
+            for i in inversos:
+                htmloperacion+="\t\t\t\t"+str(i)+" <br>\n"
+            htmloperacion+="\t\t\t\t<br>\n"
+        if len(ftrigo)>0:
+            htmloperacion+="\t\t\t\tOPERACIONES TRIGONOMETRICAS: <br>\n"
+            for i in ftrigo:
+                htmloperacion+="\t\t\t\t"+str(i)+" <br>\n"
+            htmloperacion+="\t\t\t\t<br>\n"
+        if len(suma)>0:
+            htmloperacion+="\t\t\t\tSUMA: <br>\n"
+            for i in range(len(suma)):
+                cadope+=str(suma[i])+"+"
+            Sum=sum(suma)
+            cadope=cadope[:-1]
+            cadope+="="+str(Sum)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+
+        if len(resta)>0:
+            htmloperacion+="\t\t\t\tRESTA: <br>\n"
+            aux=resta[0]
+            a=0
+            cadope+=str(aux)+"-"
+            for i in range(len(resta)):
+                if i>0:
+                    a=aux-resta[i]
+                    aux=a
+                    cadope+=str(resta[i])+"-"
+            cadope=cadope[:-1]
+            cadope+="="+str(a)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+        if len(multi)>0:
+            htmloperacion+="\t\t\t\tMULTIPLICACION: <br>\n"
+            for i in range(len(multi)):
+                cadope+=str(multi[i])+"*"
+            Multi=numpy.prod(multi)
+            cadope=cadope[:-1]
+            cadope+="="+str(Multi)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+        if len(div)>0:
+            htmloperacion+="\t\t\t\tDIVISION: <br>\n"
+            Div=div[0]/div[1]
+            cadope=str(div[0])+"/"+str(div[1])
+            cadope+="="+str(Div)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+
+        if len(potencia)>0:
+            htmloperacion+="\t\t\t\tPOTENCIA: <br>\n"
+            aux=potencia[0]
+            a=0
+            cadope+=str(aux)+"^"
+            for i in range(len(potencia)):
+                if i>0:
+                    a=aux**potencia[i]
+                    aux=a
+                    cadope+=str(potencia[i])+"^"
+            cadope=cadope[:-1]
+            cadope+="="+str(a)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+
+        if len(raiz)==2:
+            htmloperacion+="\t\t\t\tRAIZ: <br>\n"
+            a=pow(raiz[0],1/raiz[1])
+            cadope+="("+str(raiz[1])+")"+"√"+str(raiz[0])+"="+str(a)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+        if len(modulos)>1:
+            htmloperacion+="\t\t\t\tMODULO: <br>\n"
+            aux=modulos[0]
+            a=0
+            cadope+=str(aux)+"%"
+            for i in range(len(modulos)):
+                if i>0:
+                    a=aux % modulos[i]
+                    aux=a
+                    cadope+=str(modulos[i])+"%"
+            cadope=cadope[:-1]
+            cadope+="="+str(a)
+            htmloperacion+="\t\t\t\t"+cadope+" <br><br>\n"
+        cadope=""
+        htmloperacion+="""\t\t\t</div>
+    \t\t</body>
+    \t</html>"""
+        potencia.clear()
+        suma.clear()
+        resta.clear()
+        multi.clear()
+        ftrigo.clear()
+        div.clear()
+        modulos.clear()
+        inversos.clear()
+        pos.clear()
+        raiz.clear()
+        f = open('OPERACIONES_202100123.html','w')
+        f.write(htmloperacion)
+        f.close()
+        messagebox.showinfo("","Tabla de Operaciones generada con exito")
+        #time.sleep(2)
+        webbrowser.open_new_tab('OPERACIONES_202100123.html')
 
 def guardar_token(fila, columna, lexema):
     nuevotoken = Token(fila, columna, lexema)
@@ -478,7 +817,7 @@ def abrirerrores():
             "No hay archivo", "No hay archivo de lectura cargado en el sistema.")
         pressanalizar = False
     elif len(errores) == 0 and pressanalizar == False:
-        messagebox.showerror("Error!", "Necesita analizar el archivo primero.")
+        messagebox.showwarning("Ojo!", "Necesita analizar el archivo primero.")
         pressanalizar = False
     elif len(errores) == 0 and pressanalizar == True:
         messagebox.showinfo(
@@ -757,6 +1096,5 @@ def Main():
     bt_ayuda.config(image=img_ayuda)
     bt_ayuda.place(x=560, y=370)
     window_main.mainloop()
-
 
 Main()
